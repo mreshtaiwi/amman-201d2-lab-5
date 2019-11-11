5bd3d4d3dc54b36372145e5e83a190f8e79fc244
mreshtaiwi <mr.eshtaiwi@gmail.com>
2019-11-11 15:30:36 +0200

    solve the 5th problem

--- app.js
+++ app.js
@@ -113,19 +113,22 @@ sumArray(testArray);
 Write a function called multiplyArray() that takes an array of numbers as its argument and returns an array whose first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The numbers 2,3,4 have a product of 24."
-
+ The numbers 2,3,4 have a product of 24.
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. This function should handle an array containing three elements. However, you may continue to use the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+    var multiples = multiply(testArray[0],testArray[1])[0] * testArray[2] ;
+    var mmessages = "The numbers " + testArray[0] + "," + testArray[1] + "," + testArray[2] + " have a product of " + multiples + ".";
+    var result = [multiples,mmessages]
+    return result;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
-
+testMultiplyArray(testArray);
+multiplyArray(testArray);
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
